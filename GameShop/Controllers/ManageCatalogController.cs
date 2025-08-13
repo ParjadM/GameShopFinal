1f6af2e6d7576215925de95bc499cfa5a496aac8
Samuel Owusu <samuelnii6000@gmail.com>
2025-08-13 01:35:46 -0400

    Redesign the register Page

---
+++ GameShop/Controllers/ManageCatalogController.cs
@@ -0,0 +1,21 @@
+﻿using GameShop.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameShop.Controllers
+{
+    public class ManageCatalogController : Controller
+    {
+        private readonly IGameService _gameService;
+
+        public ManageCatalogController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var games = await _gameService.GetAllGamesAsync();
+            return View(games); 
+        }
+    }
+}
